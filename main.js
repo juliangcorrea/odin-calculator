3e3cf641e549c63d61225eddaf954b92c5c9585e
Julian Correa <juliangcorrea@hotmail.com>
2024-12-14 16:23:58 -0300

    Add funcitonality to input numbers into calculator + clear all numbers

--- main.js
+++ main.js
@@ -28,3 +28,27 @@ function operate(number1, number2, operator){
             return divideOperator(number1, number2)
     }
 }
+
+let btnNum = ''
+let screenNum = document.querySelector('.show-number')
+const numbersBtns = document.querySelectorAll('.number')
+const operatorBtns = document.querySelectorAll('.operator')
+const clearBtn = document.querySelector('.clear')
+const equalBtn = document.querySelector('.equal')
+numbersBtns.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(btnNum.length < 19){
+            if(btnNum != 0){
+                btnNum += e.target.textContent
+            } else {
+                btnNum = e.target.textContent
+            }
+            screenNum.textContent = btnNum
+        }
+    });
+})
+
+clearBtn.addEventListener('click', ()=>{
+    btnNum = ''
+    screenNum.textContent = 0
+})
