bbc2475bcb9f739e52a3ef4d2da21b80544ad6a2
Julian Correa <juliangcorrea@hotmail.com>
2024-12-14 14:28:22 -0300

    Add basic JS logic and basic HTML template. Update ReadMe file.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # odin-calculator
+
+Calculator project:
+
+Steps:
+
+1.- Create functions for all basic operators.
+2.- Create a function for calculator number + operator logic.
+3.- Create html buttons.
+4.- Create function to display numbers in html.
---
+++ main.js
@@ -0,0 +1,16 @@
+function addOperator(item1, item2){
+    return item1 + item2
+}
+
+function subtractOperator(item1, item2){
+    return item1 - item2
+}
+
+function multiplyOperator(item1, item2){
+    return item1 * item2
+}
+
+function divideOperator(item1, item2){
+    return item1 / item2
+}
+
