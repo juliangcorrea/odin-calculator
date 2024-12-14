72f9f400bc910411ec59387ee6e41d35007c2b04
Julian Correa <juliangcorrea@hotmail.com>
2024-12-14 15:07:21 -0300

    Add medium operator logic to JS (using different operators). Add basic styling and better structure to HTML

--- main.js
+++ main.js
@@ -14,3 +14,17 @@ function divideOperator(item1, item2){
     return item1 / item2
 }
 
+let number1, number2, operator
+
+function operate(number1, number2, operator){
+    switch(operator){
+        case "+":
+            return addOperator(number1, number2)
+        case "-":
+            return subtractOperator(number1, number2)
+        case "*":
+            return multiplyOperator(number1, number2)
+        case "/":
+            return divideOperator(number1, number2)
+    }
+}
